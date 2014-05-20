5487447e7d60d3591582f06d010e6ac4c77b15ba
Joan <joan@Joan-iMac.local>
2014-05-20 20:28:11 +0200

    test

--- setup.js
+++ setup.js
@@ -5,6 +5,6 @@ $(document).ready(function(){
 		qstrings: {
 			id: '87792318@N00'
 		},
-		itemTemplate: '<li><img src="{{image_c}}" alt="" /></li>'
+		itemTemplate: '<li><img src="{{image_z}}" alt="" /></li>'
 	});
 });
