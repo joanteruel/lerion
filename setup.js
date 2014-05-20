e4102fdc32ed735e34004bb22d636693fc0b4452
Joan <joan@Joan-iMac.local>
2014-05-20 19:59:43 +0200

    test

--- setup.js
+++ setup.js
@@ -7,16 +7,4 @@ $(document).ready(function(){
 		},
 		itemTemplate: '<li><img src="{{image_z}}" alt="" /></li>'
 	});
-	
-	$('#nocallback').jflickrfeed({
-		limit: 2,
-		qstrings: {
-			id: '87792318@N00'
-		},
-		useTemplate: false,
-		itemCallback: function(item){
-			$(this).append("<li><img src='" + item.image_z + "' alt=''/></li>");
-		}
-	});
-
 });
